1f55924812a497beff14adc14b9422c7cc2ff54f
Nicholas Robbins <nl7robbins@icloud.com>
2023-04-21 21:08:20 -0700

    Finishing touches

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ import { Server }from "socket.io";
 const io = new Server({
     // WARNING INSECURE: Adjust these settings for any non-local env
     cors: {
-        origin:"http://localhost:3000",
+        origin: "*",
     }
 });
 
 io.on("connection", (socket) => {
     console.log("New connection to the chat server");
-});
-
-io.on("chatMessage", (data) => {
-    console.log("🚀 ~ file: server.js:15 ~ io.on ~ data:", data)
+    socket.on("chatMessageFromClient", (data) => {
+        io.emit("chatMessageFromServer", data);
+    });
 });
 
 io.listen(3000);
